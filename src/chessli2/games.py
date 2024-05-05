135d82425f57228265eaeadd9a6d0d85e9207f94
Pascal Wenker <pwenker@posteo.de>
2024-05-05 11:39:24 +0200

    Prepare HF space

--- src/chessli2/games.py
+++ src/chessli2/games.py
@@ -8,7 +8,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env")
-    lichess_api_token: str
+    lichess_api_token: str = ''
 
 
 settings = Settings()
